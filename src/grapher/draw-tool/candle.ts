8fa57b0f35176c4cf40a4cc2dc45ff12475b3b0e
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2022-10-15 19:16:35 +0400

    Draw candle with margin and in the center of cell

--- src/grapher/draw-tool/candle.ts
+++ src/grapher/draw-tool/candle.ts
@@ -1,5 +1,5 @@
 import { Canvas } from "../canvas";
-import { InputJSONArray } from "../grapher";
+import { Grapher, InputJSONArray } from "../grapher";
 import { Layer } from "../layer";
 import { Range } from "../utils/range";
 import { DrawTool } from "./base";
@@ -14,8 +14,9 @@ export class Candle implements DrawTool{
         const data = layer.data
         let cursorX = layer.grapher.canvas.source.width
         for (const candle of data.reverse()) {
+            this.draw_candle_wick(cursorX, candle, layer)
             this.draw_candle_body(cursorX, candle, layer)
-            cursorX -= layer.grapher.unit_interval
+            cursorX -= layer.grapher.cell_width
         }
     }
 
@@ -24,14 +25,27 @@ export class Candle implements DrawTool{
         const open = y_axis.toPixel(candle['open'])
         const close = y_axis.toPixel(candle['close'])
         const ctx = layer.grapher.canvas.context
+        const pixel_marging = Grapher.CELL_MARGIN_PERCENT * layer.grapher.cell_width
+        const x = cursorX - pixel_marging
+        const width = -(layer.grapher.cell_width - pixel_marging*2)
+        
         ctx.fillStyle = (open > close) ? 'red' : 'green';
-        ctx.fillRect(cursorX, open, layer.grapher.unit_interval, close - open)
-        // console.log(close-open, cursorX)
+        ctx.fillRect(x, open, width, close - open)
     }
 
-    
-
-    
+    draw_candle_wick(cursorX: number, candle: Map<String, any>, layer: Layer) { 
+        const y_axis = layer.grapher.y_axis
+        const high = y_axis.toPixel(candle['high'])
+        const low = y_axis.toPixel(candle['low'])
+        const ctx = layer.grapher.canvas.context
+        const cell_width = layer.grapher.cell_width
+        ctx.strokeStyle = 'gray'
+        ctx.lineWidth = 1
+        ctx.beginPath()
+        ctx.moveTo(cursorX - cell_width/2, high)
+        ctx.lineTo(cursorX - cell_width/2, low)
+        ctx.stroke()
+    }
 
     getRange(input: InputJSONArray): Range {
         let min = Number.MAX_SAFE_INTEGER
--- src/grapher/grapher.ts
+++ src/grapher/grapher.ts
@@ -1,4 +1,5 @@
 import { Canvas } from "./canvas"
+import { Interaction } from "./interaction"
 import { Layer } from "./layer"
 import { Range } from "./utils/range"
 import { VirtualAxis } from "./virtual-axis"
@@ -6,18 +7,21 @@ import { VirtualAxis } from "./virtual-axis"
 export type InputJSONArray = Array<Map<String, any>>
 
 export class Grapher {
+    static readonly CELL_WIDTH = 20
+    static readonly CELL_MARGIN_PERCENT = 0.2
     canvas: Canvas
     layers: Array<Layer>
-    interval: number
     y_axis: VirtualAxis
-    data_count = 0
-    unit_interval = 0
+    cell_width = Grapher.CELL_WIDTH
+    scroll_callbacks: Array<Function> = []
+    interaction: Interaction
+    
 
     constructor(canvas: Canvas) {
         this.canvas = canvas
         this.layers = []
         this.y_axis = new VirtualAxis(new Range(0, canvas.source.height))
-        this.unit_interval = 0
+        this.interaction = new Interaction(this.canvas)
     }
     
     add(layer: Layer): void {
@@ -26,14 +30,27 @@ export class Grapher {
         virtual_range = Range.extremum(virtual_range, layer.range)
         this.y_axis.virtual_range = virtual_range
         this.layers.push(layer)
-        this.data_count = Math.max(this.unit_interval, layer.data.length)
-        this.unit_interval = this.canvas.source.width / this.data_count
+        // this.cell_width = this.canvas.source.width / this.get_max_data_length()
+    }
+
+    get_max_data_length(): number {
+        const max_layer = this.layers.reduce((p, c) => (p.data.length > c.data.length)? p: c)
+        return max_layer.data.length
     }
 
     update(): void {
         this.layers.forEach(layer => {
             layer.tool.draw(layer, this.canvas)
         });        
+    }
+
+    handle_scroll_event() {
+        
+    }
+
+    
+    subscribe_to_scroll(scroll_callback: Function): void {
+        this.scroll_callbacks.push(scroll_callback)
     }
 
 
