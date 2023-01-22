ec7cba89d949dc1eeb296dedd86f5b5f5ecb3c6d
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2023-01-22 23:00:50 +0100

    Fix a bug in interval model test

--- test/interval_model_test.dart
+++ test/interval_model_test.dart
@@ -29,13 +29,14 @@ void main() {
 
   group("Interval model loading ->", () {
     test("Assert loading is done asynchronously at instanciation", () async {
-      // Read provider to activate model instanciation (lazy loading)
+      // Read provider to activate model instanciation (lazy)
       providerContainer.read(intervalModelProvider);
       verify(() => mockPreference.load('interval')).called(1);
     });
     test("Expect loading is delegate to PreferencIO", () async {
       await providerContainer.read(intervalModelProvider.notifier).refresh();
-      verify(() => mockPreference.load('interval')).called(1);
+      // Called 2 times at init + here with refresh
+      verify(() => mockPreference.load('interval')).called(2);
     });
 
     test("Assert state is retrieved by loading preference ", () async {
