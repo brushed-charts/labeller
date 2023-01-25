e68e7a533a4216cb286b6a775850849530757fd2
olivierpicard <32243829+olivierpicard@users.noreply.github.com>
2023-01-25 09:04:39 +0100

    [Not working] tmp commit

--- lib/conditionnal_chart_view.dart
+++ lib/conditionnal_chart_view.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:labelling/model/date_range_model.dart';
+import 'package:labelling/model/interval_model.dart';
+import 'package:labelling/model/source_model.dart';
 
-final chartViewProvider = Provider((ref) {
-  ref.pro continue here 
+enum ChartViewState { noData, loading, error, onData }
+
+final isChartMetadataReady = Provider<bool>((ref) {
+  final source = ref.read(sourceModelProvider.notifier);
+  final interval = ref.read(intervalModelProvider.notifier);
+  final dateRange = ref.read(dateRangeProvider.notifier);
+  if (!source.isLoaded || !interval.isLoaded || !dateRange.isLoaded) {
+    return false;
+  }
+  return true;
+});
+
+final chartViewStateProvider = Provider((ref) {
+  final source = ref.watch(sourceModelProvider);
+  final interval = ref.watch(intervalModelProvider);
+  final dateRange = ref.watch(dateRangeProvider);
 });
 
 class ConditionnalChartView extends ConsumerWidget {
