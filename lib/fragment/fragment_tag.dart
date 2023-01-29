b7000b607157227041a0b38b437a2c301cb0b5ea
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2023-01-29 22:51:19 +0100

    Associate a name with fragment using FragmentTag.

---
+++ lib/fragment/fragment_tag.dart
@@ -0,0 +1,8 @@
+import 'package:labelling/fragment/base.dart';
+
+class FragmentTag {
+  final String name;
+  final FragmentContract fragment;
+
+  FragmentTag({required this.name, required this.fragment});
+}
