42988ec3aec6d3fb853b263d951b01490e24431c
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2023-02-12 22:05:28 +0100

    Add a controller for model and resolver interaction

--- lib/fragment/fragment_controller.dart
+++ lib/fragment/fragment_controller.dart
@@ -1,5 +1,8 @@
+import 'package:grapher/kernel/object.dart';
+import 'package:labelling/fragment/fragment_interface.dart';
 import 'package:labelling/fragment/fragment_model.dart';
 import 'package:labelling/fragment/fragment_resolver_interface.dart';
+import 'package:logging/logging.dart';
 
 class FragmentController {
   FragmentController(
@@ -10,4 +13,16 @@ class FragmentController {
 
   final FragmentModel _model;
   final FragmentResolverInterface _resolver;
+  final Logger _logger = Logger("fragment_controller");
+
+  void add(FragmentInterface fragment) {
+    _logger.finest("Adding to model fragment "
+        "(${fragment.name} whith hash ${fragment.hashCode})");
+    _model.add(fragment);
+  }
+
+  GraphObject toGraphObject() {
+    _logger.finest("Begin the convertion of model fragment to graphObject");
+    return _resolver.reduceToGraphObject(_model);
+  }
 }
