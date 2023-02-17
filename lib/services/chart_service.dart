d28e3cc8c717509aa990c5edc45e6697b69b4748
Olivier Picard <32243829+olivierpicard@users.noreply.github.com>
2023-02-17 22:51:46 +0100

    Add reference repository to chart service

--- lib/services/chart_service.dart
+++ lib/services/chart_service.dart
@@ -1,12 +1,17 @@
+import 'package:grapher/reference/contract.dart';
+import 'package:grapher/reference/memory_repository.dart';
 import 'package:labelling/query/graphql/gql_query.dart';
 import 'package:labelling/query/market_query_contract.dart';
 
 class ChartService {
-  ChartService({required this.marketQuery});
+  ChartService({required this.marketQuery, required this.referenceRepository});
 
   final MarketQuery marketQuery;
+  final ReferenceRepositoryInterface referenceRepository;
 
   factory ChartService.initWithDefault() {
-    return ChartService(marketQuery: GQLQuery.initWithDefaultValue());
+    return ChartService(
+        marketQuery: GQLQuery.initWithDefaultValue(),
+        referenceRepository: ReferenceRepositoryInMemory());
   }
 }
